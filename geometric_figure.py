4a1357a567d4c62f17f2dfdccd3ec70f2af0e22a
SaushkinDenis <saushkinden@mail.ru>
2020-01-30 22:04:50 +0400

    Реализация каждой фигуры

--- geometric_figure.py
+++ geometric_figure.py
@@ -1,47 +1,81 @@
-class Geometric_Figure:
+import math
 
-    def __init__(self, name, area, angles, perimeter):
+
+class Geometric_figure:
+
+    def __init__(self, name, sides):
         self.name = name
-        self.area = area
-        self.angles = angles
-        self.perimeter = perimeter
+        self.sides = sides
 
     def get_area(self):
-        return self.area
+        return self.sides[0] * self.sides[1]
 
     def get_angles(self):
-        return self.angles
+        return 4
 
     def get_perimeter(self):
-        return self.perimeter
+        area = 0
+        for i in self.sides:
+            area += i
+        return area
 
     def add_area(self, other):
-        if isinstance(other, Geometric_Figure):
-            return self.perimeter + other.get_perimeter()
+        if isinstance(other, Geometric_figure):
+            return self.get_perimeter() + other.get_perimeter()
         else:
-            raise ("Передан неправильный класс!")
+            raise RuntimeError("Передан неправильный класс!")
 
 
-class Triangle(Geometric_Figure):
-    def __init__(self, area, perimeter):
-        super().__init__("Triangle", area, 3, perimeter)
+class Triangle(Geometric_figure):
+    def __init__(self, sides):
+        if len(sides) == 3:
+            super().__init__("Triangle", sides)
+        else:
+            raise RuntimeError("Количество сторон отлично от 3")
+
+    def get_area(self):
+        p = self.get_perimeter() / 2
+        return round(math.sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2])), 1)
+
+    def get_angles(self):
+        return 3
 
 
-class Rectangle(Geometric_Figure):
-    def __init__(self, area, perimeter):
-        super().__init__("Rectangle", area, 4, perimeter)
+class Square(Geometric_figure):
+    def __init__(self, sides):
+        if len(sides) == 4:
+            super().__init__("Square", sides)
+        else:
+            raise RuntimeError("Количество сторон отлично от 4")
 
 
-class Square(Geometric_Figure):
-    def __init__(self, area, perimeter):
-        super().__init__("Square", area, 4, perimeter)
+class Rectangle(Geometric_figure):
+    def __init__(self, sides):
+        if len(sides) == 4:
+            super().__init__("Rectangle", sides)
+        else:
+            raise RuntimeError("Количество сторон отлично от 4")
 
 
-class Circle(Geometric_Figure):
-    def __init__(self, area, perimeter):
-        super().__init__("Circle", area, 0, perimeter)
+class Circle(Geometric_figure):
+    def __init__(self, radius):
+        super().__init__("Circle", radius)
 
 
-a = Triangle(1, 1)
-b = Triangle(1, 1)
-print(a.add_area(b))
+
+    def get_area(self):
+        return round(math.pi * (self.sides[0] ** 2), 1)
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.sides[0]
+
+    def get_angles(self):
+        return 0
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.sides[0]
+
+
+a = Circle([1])
+
+print(a.get_perimeter())
