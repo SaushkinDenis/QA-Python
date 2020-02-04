a267724e3de4811676a6311528d29ee67742fe89
SaushkinDenis <saushkinden@mail.ru>
2020-02-04 11:43:33 +0400

    Усовершенствован код в плане ООП.

--- geometric_figure.py
+++ geometric_figure.py
@@ -4,19 +4,19 @@ from functools import reduce
 
 class Geometric_figure:
 
-    def __init__(self, name, sides, angles):
+    def __init__(self, name, params, angles):
         self.name = name
-        self.sides = sides
+        self.params = params
         self.angles = angles
 
     def get_area(self):
-        return self.sides[0] * self.sides[1]
+        return reduce((lambda x, y: x * y), self.params)
 
     def get_angles(self):
         return self.angles
 
     def get_perimeter(self):
-        return reduce((lambda x, y: x + y), self.sides)
+        return reduce((lambda x, y: x + y), self.params)
 
     def add_area(self, other):
         if isinstance(other, Geometric_figure):
@@ -28,45 +28,46 @@ class Geometric_figure:
 class Triangle(Geometric_figure):
     __name = "Triangle"
 
-    def __init__(self, sides):
+    def __init__(self, __name, sides):
         if len(sides) == 3:
-            super().__init__(self.__name, sides, 3)
+            super().__init__(__name, sides, 3)
         else:
             raise RuntimeError("Количество сторон отлично от 3")
 
     def get_area(self):
         p = self.get_perimeter() / 2
-        return round(math.sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2])), 1)
-
-
-class Square(Geometric_figure):
-    __name = "Square"
-
-    def __init__(self, sides):
-        if len(sides) == 4:
-            super().__init__(self.__name, sides, 4)
-        else:
-            raise RuntimeError("Количество сторон отлично от 4")
+        return round(math.sqrt(p * (p - self.params[0]) * (p - self.params[1]) * (p - self.params[2])), 1)
 
 
 class Rectangle(Geometric_figure):
     __name = "Rectangle"
 
-    def __init__(self, sides):
-        if len(sides) == 4:
-            super().__init__(self.__name, sides, 4)
+    def __init__(self, __name, sides):
+        if len(sides) == 2:
+            super().__init__(__name, sides, 4)
         else:
-            raise RuntimeError("Количество сторон отлично от 4")
+            raise RuntimeError("Количество сторон отлично от 2")
+
+    def get_perimeter(self):
+        return (self.params[0] + self.params[1]) * 2
+
+
+class Square(Rectangle):
+    __name = "Square"
+
+    def __init__(self, __name, side):
+        super().__init__(__name, [side, side])
 
 
 class Circle(Geometric_figure):
     __name = "Circle"
 
     def __init__(self, radius):
+        print(radius)
         super().__init__(self.__name, radius, 0)
 
     def get_area(self):
-        return round(math.pi * (self.sides[0] ** 2), 1)
+        return round(math.pi * (self.params ** 2), 1)
 
     def get_perimeter(self):
-        return 2 * math.pi * self.sides[0]
+        return 2 * math.pi * self.params
--- tests/test_geometric_figure.py
+++ tests/test_geometric_figure.py
@@ -1,13 +1,10 @@
-import pytest
-
-from geometric_figure import Triangle
-
-
 class TestTriangle:
-    def test_get_area(self):
-        a = Triangle([1, 2, 4])
-        print(a.get_area())
 
+    def test_get_name(self):
+        pass
+
+    def test_get_area(self):
+        pass
 
     def test_get_angles(self):
         pass
@@ -19,14 +16,52 @@ class TestTriangle:
         pass
 
 
-class TestTriangle:
+class TestRectangle:
+    def test_get_name(self):
+        pass
+
     def test_get_area(self):
+        pass
+
+    def test_get_angles(self):
+        pass
+
+    def test_get_perimeter(self):
+        pass
+
+    def test_add_area(self):
+        pass
+
+
+class TestSquare:
+    def test_get_name(self):
+        pass
+
+    def test_get_area(self):
+        pass
+
+    def test_get_angles(self):
+        pass
+
+    def test_get_perimeter(self):
+        pass
+
+    def test_add_area(self):
         pass
 
 
 class TestCircle:
+    def test_get_name(self):
+        pass
+
     def test_get_area(self):
         pass
 
+    def test_get_angles(self):
+        pass
+
     def test_get_perimeter(self):
+        pass
+
+    def test_add_area(self):
         pass
