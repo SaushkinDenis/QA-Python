dde4693c8657ac171f4f80300feccf58f3ef5b1d
SaushkinDenis <saushkinden@mail.ru>
2020-02-04 09:00:48 +0400

    + Начальная структура тестирования

--- geometric_figure.py
+++ geometric_figure.py
@@ -1,23 +1,22 @@
 import math
+from functools import reduce
 
 
 class Geometric_figure:
 
-    def __init__(self, name, sides):
+    def __init__(self, name, sides, angles):
         self.name = name
         self.sides = sides
+        self.angles = angles
 
     def get_area(self):
         return self.sides[0] * self.sides[1]
 
     def get_angles(self):
-        return 4
+        return self.angles
 
     def get_perimeter(self):
-        area = 0
-        for i in self.sides:
-            area += i
-        return area
+        return reduce((lambda x, y: x + y), self.sides)
 
     def add_area(self, other):
         if isinstance(other, Geometric_figure):
@@ -27,9 +26,11 @@ class Geometric_figure:
 
 
 class Triangle(Geometric_figure):
+    __name = "Triangle"
+
     def __init__(self, sides):
         if len(sides) == 3:
-            super().__init__("Triangle", sides)
+            super().__init__(self.__name, sides, 3)
         else:
             raise RuntimeError("Количество сторон отлично от 3")
 
@@ -37,45 +38,35 @@ class Triangle(Geometric_figure):
         p = self.get_perimeter() / 2
         return round(math.sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2])), 1)
 
-    def get_angles(self):
-        return 3
-
 
 class Square(Geometric_figure):
+    __name = "Square"
+
     def __init__(self, sides):
         if len(sides) == 4:
-            super().__init__("Square", sides)
+            super().__init__(self.__name, sides, 4)
         else:
             raise RuntimeError("Количество сторон отлично от 4")
 
 
 class Rectangle(Geometric_figure):
+    __name = "Rectangle"
+
     def __init__(self, sides):
         if len(sides) == 4:
-            super().__init__("Rectangle", sides)
+            super().__init__(self.__name, sides, 4)
         else:
             raise RuntimeError("Количество сторон отлично от 4")
 
 
 class Circle(Geometric_figure):
+    __name = "Circle"
+
     def __init__(self, radius):
-        super().__init__("Circle", radius)
-
-
+        super().__init__(self.__name, radius, 0)
 
     def get_area(self):
         return round(math.pi * (self.sides[0] ** 2), 1)
 
     def get_perimeter(self):
         return 2 * math.pi * self.sides[0]
-
-    def get_angles(self):
-        return 0
-
-    def get_perimeter(self):
-        return 2 * math.pi * self.sides[0]
-
-
-a = Circle([1])
-
-print(a.get_perimeter())
---
+++ tests/test_geometric_figure.py
@@ -0,0 +1,32 @@
+import pytest
+
+from geometric_figure import Triangle
+
+
+class TestTriangle:
+    def test_get_area(self):
+        a = Triangle([1, 2, 4])
+        print(a.get_area())
+
+
+    def test_get_angles(self):
+        pass
+
+    def test_get_perimeter(self):
+        pass
+
+    def test_add_area(self):
+        pass
+
+
+class TestTriangle:
+    def test_get_area(self):
+        pass
+
+
+class TestCircle:
+    def test_get_area(self):
+        pass
+
+    def test_get_perimeter(self):
+        pass
