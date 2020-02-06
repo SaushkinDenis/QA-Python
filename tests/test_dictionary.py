67ecac63e2e841c538b4efeeeca44ef673df472a
SaushkinDenis <saushkinden@mail.ru>
2020-02-06 10:01:47 +0400

    Поправки в стректуре и комментариях

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,12 +1,12 @@
 class TestDictionary:
 
-    # Проверка команды clear()
+    """Проверка команды clear()"""
     def test_dictionary_one(self, fixture_three, random_number):
         dict1 = fixture_three
         dict1.clear()
         assert len(dict1) == 0
 
-    # Проверка команды pop()
+    """Проверка команды pop()"""
     def test_dictionary_two(self, fixture_three):
         dictionary = fixture_three
         count_values = 0
@@ -15,11 +15,11 @@ class TestDictionary:
             count_values += 1
         assert count_values == 2
 
-    # Проверка команды get()
+    """Проверка команды get()"""
     def test_dictionary_three(self, fixture_three):
         assert fixture_three.get("Size") == 10
 
-    # Проверка команды update()
+    """Проверка команды update()"""
     def test_dictionary_four(self, fixture_three):
         fixture_three.update({"Size": 11, "Ex": "null"})
         assert fixture_three == {
@@ -29,6 +29,6 @@ class TestDictionary:
             "Ex": "null"
             }
 
-    # Проверка команды setdefault()
+    """Проверка команды setdefault()"""
     def test_dictionary_five(self, fixture_three):
         assert fixture_three.setdefault("Ex", "null") == fixture_three.get("Ex")
--- tests/test_list.py
+++ tests/test_list.py
@@ -1,27 +1,27 @@
 class TestList:
 
-    # Проверка команды append()
+    """Проверка команды append()"""
     def test_list_one(self, fixture_one, random_number):
         l1 = fixture_one
         l1.append(random_number)
         assert len(l1) == 6
 
-    # Проверка команды clear()
+    """Проверка команды clear()"""
     def test_list_two(self, fixture_one):
         l1 = fixture_one
         l1.clear()
         assert len(l1) == 0
 
-    # Проверка команды insert()
+    """Проверка команды insert()"""
     def test_list_three(self, fixture_one, random_number):
         l1 = fixture_one
         l1.insert(0, random_number)
         assert l1[0] == random_number
 
-    # Проверка команды pop()
+    """Проверка команды pop()"""
     def test_list_four(self, fixture_one):
         assert fixture_one.pop(0) == 1
 
-    # Проверка команды count()
+    """Проверка команды count()"""
     def test_list_five(self, fixture_one):
         assert fixture_one.count(1) == 1
