a197e067299a4aaaae185ed6ccbfbfacddc24556
sauskindenis <saushkinden@mail.ru>
2020-03-10 15:37:28 +0400

    Тестирование с ожиданиями элементов

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="safari", help="Web Browser")
+    parser.addoption("--wait", action="store", default="60", help="Set wait")
+
+@pytest.fixture
+def browser(request):
+    browser = request.config.getoption("--browser")
+    if browser == "safari":
+        wd = webdriver.Safari()
+    elif browser == "chrome":
+        op = webdriver.ChromeOptions()
+        wd = webdriver.Chrome(executable_path='/Users/sauskindenis/Desktop/webdrivers/chromedriver', options=op)
+    elif browser == "ie":
+        op = webdriver.IeOptions()
+        op.add_argument("headless")
+        wd = webdriver.Ie(options=op)
+    elif browser == "firefox":
+        op = webdriver.FirefoxOptions()
+        op.add_argument("headless")
+        wd = webdriver.Firefox(options=op)
+    else:
+        wd = webdriver.Safari()
+
+    wd.maximize_window()
+
+    yield wd
+    wd.quit()
+
+
+@pytest.fixture
+def waits(request):
+    wait = request.config.getoption("--wait")
+    return int(wait)
