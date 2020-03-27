a9cd1f9c976bd8bfa2b8aa4cb80c7cbd8694ab88
sauskindenis <saushkinden@mail.ru>
2020-03-27 15:39:33 +0400

    Внешний полишинг AlphaTest

--- Test/test_products.py
+++ Test/test_products.py
@@ -1,57 +1,58 @@
 import time
 
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import UnexpectedAlertPresentException
 
+from common.BasicCommand import BasicCommand
 from locators.locators import locators
 
-class Test_AdminPage():
 
-    driver = None
+class Test_Admin_Page:
 
     def test_edit_product(self, browser, waits):
-        self.driver = browser
-        self.driver.get(locators.ListProductsPage.url['css'])
 
-        self._click(locators.AdminPage.selector_auth['css'])
-        self._click(locators.ListProductsPage.selector_edit_product_first['css'])
+        driver = BasicCommand(browser)
 
-        self._input(locators.ProductPage.selector_name_product['css'], "Apple Cinema 35\"")
-        self._click(locators.ProductPage.selector_text_product_style['css'])
-        self._click(locators.ProductPage.selector_text_product_style_blockquote['css'])
-        self._click(locators.ProductPage.selector_product_save['css'])
+        driver._click(locators.ListProductsPage.selector_edit_product_first['css'])
+        driver._input(locators.ProductPage.selector_name_product['css'], "Apple Cinema 35\"")
+        driver._click(locators.ProductPage.selector_text_product_style['css'])
+        driver._click(locators.ProductPage.selector_text_product_style_blockquote['css'])
+        driver._click(locators.ProductPage.selector_product_save['css'])
 
-        # self._click(locators.ListProductsPage.selector_edit_product_first['css'])
+        driver._click(locators.AdminPage.selector_menu_catalog['css'])
+        driver._click(locators.AdminPage.selector_menu_products['css'])
+        driver._click(locators.ListProductsPage.selector_edit_product_first['css'])
 
-        assert self._text(locators.ProductPage.selector_name_product['css']) == "Apple Cinema 35\""
+        assert driver._get_attribute(locators.ProductPage.selector_name_product['css'], "value") == "Apple Cinema 35\""
 
 
     def test_add_product(self, browser, waits):
-        self.driver = browser
-        self.driver.get(locators.ListProductsPage.url['css'])
+        driver = BasicCommand(browser)
 
-        self._click(locators.AdminPage.selector_auth['css'])
+        a = driver._get_len(locators.ListProductsPage.selector_list_product['css']) + 1
 
+        driver._click(locators.ListProductsPage.selector_add_product['css'])
+        driver._input(locators.ProductPage.selector_name_product['css'], "Product #1")
+        driver._click(locators.ProductPage.selector_product_save["css"])
+        time.sleep(5)
+        driver._click(locators.AdminPage.selector_menu_catalog['css'])
+        driver._click(locators.AdminPage.selector_menu_products['css'])
+
+        assert a == driver._get_len(locators.ListProductsPage.selector_list_product['css'])
 
     def test_del_product(self, browser, waits):
-        self.driver = browser
-        self.driver.get(locators.ListProductsPage.url['css'])
+        driver = BasicCommand(browser)
 
-        self._click(locators.AdminPage.selector_auth['css'])
+        a = driver._get_len(locators.ListProductsPage.selector_list_product['css']) - 1
+        driver._click(locators.ListProductsPage.selector_flag_first_product['css'])
+        driver._click(locators.ListProductsPage.selector_del_product['css'])
+        driver._alert_accept()
+        time.sleep(5)
+        driver._click(locators.AdminPage.selector_menu_catalog['css'])
+        driver._click(locators.AdminPage.selector_menu_products['css'])
+        time.sleep(5)
 
-        
+
+        assert a ==  driver._get_len(locators.ListProductsPage.selector_list_product['css'])
 
 
-    def __element(self, selector):
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
 
-    def _click(self, selector):
-        ActionChains(self.driver).move_to_element(self.__element(selector)).click().perform()
-
-    def _input(self, selector, value="Example"):
-        self.__element(selector).send_keys(value)
-
-    def _text(self, selector):
-        return self.__element(selector).text
--- locators/locators.py
+++ locators/locators.py
@@ -2,12 +2,17 @@ class locators:
 
     class AdminPage:
         selector_auth = {'css': "div.panel-body > form > div.text-right > button"}
+        selector_menu_catalog = {'css': "#menu-catalog > a"}
+        selector_menu_products = {'css': "#collapse1 > li:nth-child(2) > a"}
 
     class ListProductsPage:
         url = {'css': 'https://demo.opencart.com/admin/index.php?route=catalog/product&user_token=vfru3mBkQg3TPmtnnD9wSBYYA8wCVXST'}
+
         selector_edit_product_first = {'css': "#form-product > div > table > tbody > tr:nth-child(1) > td:nth-child(8) > a > i"}
         selector_add_product = {'css': "#content > div.page-header > div > div > a"}
-        selector_flag_product = {'css': "#form-product > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > input[type='checkbox']"}
+        selector_flag_first_product = {'css': "#form-product > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > input[type='checkbox']"}
+        selector_list_product = {'css': "#form-product > div > table > tbody > tr"}
+        selector_del_product = {'css': "#content > div.page-header > div > div > button.btn.btn-danger"}
 
     class ProductPage:
         selector_name_product = {'css': "#input-name1"}
